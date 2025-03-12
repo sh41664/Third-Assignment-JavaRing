13b2b03dad37abf84d940cea5062fc1ebc9b4798
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2025-03-12 20:33:01 +0330

    feat(game): updated main file

--- Java-Ring/src/main/java/org/project/Main.java
+++ Java-Ring/src/main/java/org/project/Main.java
@@ -1,8 +1,15 @@
 package org.project;
 
+import org.project.location.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        //TODO: IMPLEMENT GAMEPLAY
+    public static void main(String[] args) {
+        // TODO: ADD SOME LOCATIONS TO YOUR GAME
+        List<Location> locations = new ArrayList<>();
+
+        // TODO: IMPLEMENT GAMEPLAY
     }
 }
